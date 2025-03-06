19e9fe61fa3dbea97bdf7d07d81522f5637ea898
nileshahirrao10 <nileshahirrao10@gmail.com>
2025-03-06 23:20:52 +0530

    Exception is added

--- demo.py
+++ demo.py
@@ -1,7 +1,24 @@
-from src.logger import logging
+#from src.logger import logging
 
-logging.debug("This is debug message")
-logging.info("This is info message")
-logging.warning("This is warning message")
-logging.error("This is error message")
-logging.critical("This is critical message")
+#logging.debug("This is debug message")
+#logging.info("This is info message")
+#logging.warning("This is warning message")
+#logging.error("This is error message")
+#logging.critical("This is critical message")
+
+
+
+
+
+#######################---------------------------------####################
+
+
+from src.logger import logging
+from src.exception import MyException
+import sys
+
+try:
+    a=1+'Z'
+except Exception as e:
+    raise MyException(e,sys) from e    
+
