0194959fd8b72ae5852b615be3907cac54e3ff3f
nileshahirrao10 <nileshahirrao10@gmail.com>
2025-03-25 00:32:10 +0530

    Data Transformation Done Successfully

--- src/utils/main_utils.py
+++ src/utils/main_utils.py
@@ -26,7 +26,7 @@ def write_yaml_file(file_path:str,content:object,replace:bool=False)->None:
             if os.path.exists(file_path):
                 os.remove(file_path)
             os.makedirs(os.path.dirname(file_path),exist_ok=True)
-            with open(file_path,"w") as file:
+            with open(file_path,'w') as file:
                 yaml.dump(content,file)
     except Exception as e:
         raise MyException(e,sys) from e
@@ -44,7 +44,7 @@ def load_object(file_path:str)->object:
 def save_object(file_path:str,obj:object)->None:
     try:
         os.makedirs(os.path.dirname(file_path),exist_ok=True)   
-        with open(file_path,"w") as obj_file:
+        with open(file_path,"wb") as obj_file:
             dill.dump(obj,obj_file)
     except Exception as e:
         raise MyException(e,sys) from e
@@ -53,7 +53,7 @@ def save_object(file_path:str,obj:object)->None:
 
 def load_numpy_array_data(file_path:str)->np.array:
     try:
-        with open(file_path,"rb") as array_file:
+        with open(file_path,'rb') as array_file:
             return np.load(array_file)
     except Exception as e:
         raise MyException(e,sys) from e
@@ -63,14 +63,7 @@ def save_numpy_array_data(file_path:str,array:np.array):
     try:
         dir_path=os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
-        with open(file_path,"w") as file_obj:
+        with open(file_path,'wb') as file_obj:
             return np.save(file_obj,array)
     except Exception as e:
         raise MyException(e,sys) from e
-
-
-
-        
-        
-            
-
